76184504992859d7ad7d65b7633cf0c65202b0ea
vadim-novikov1 <vadimnovikov178@gmail.com>
2024-11-21 14:41:37 +0300

    Add files via upload

---
+++ лабораторная работа №2/продолжение финансового планирования.py
@@ -0,0 +1,11 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+money_capital = 0
+for _ in range(months):
+    money_capital += spend - salary
+    spend = spend + (spend * increase)
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
